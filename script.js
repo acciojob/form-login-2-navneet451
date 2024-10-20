7f998a0d16560d3bf827e3648baf41633e2ec14d
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-20 09:59:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function showAlert() {
+            // Get the values from the form fields
+            var firstName = document.getElementById("firstName").value;
+            var lastName = document.getElementById("lastName").value;
+            var phoneNumber = document.getElementById("phoneNumber").value;
+            var emailID = document.getElementById("emailID").value;
+
+            // Format the alert message
+            var alertMessage = "First Name: " + firstName + " " +
+                               "Last Name: " + lastName + " " +
+                               "Phone Number: " + phoneNumber + " " +
+                               "Email ID: " + emailID;
+
+            // Show the alert with the form information
+            alert(alertMessage);
+        }
